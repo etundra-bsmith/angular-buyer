658fbfd16c846f5dc8b3628b4bb103f4c235842c
Crhistian Ramirez <cramirez@four51.com>
2017-10-03 16:01:06 -0500

    add error logging to scheduler

--- scheduled-approvals-check.js
+++ scheduled-approvals-check.js
@@ -6,16 +6,18 @@ var _ = require('underscore');
 var fs = require('fs');
 var dateformat = require('dateformat');
 var buyerid = JSON.parse(fs.readFileSync('./src/app/app.constants.json')).buyerid;
+var chalk = require('chalk');
 
 var mandrill = require('mandrill-api/mandrill');
 var mandrillConfig = require('./routes/config/mandrill');
 var mandrill_client = new mandrill.Mandrill(mandrillConfig.apiKey);
 
-/*  Config Error Handling   */
-if(!buyerid) console.error(new Error('Please define buyerid in src/app/app.constants.json'));
-if(!boconfig.ClientID) console.error(new Error('missing required ClientID for back office user'));
-if(!boconfig.ClientSecret) console.error(new Error('missing required ClientSecret for back office user'));
-if(!boconfig.scope) console.error(new Error('missing required scope for back office user'));
+cLog('Preliminary Diagnostics: Checking Config');
+if(!buyerid) return cError('Missing buyerid from src/app/app.constants.json - EXIT PROCESS');
+if(!boconfig.ClientID) return cError('Missing ClientID for back office user from routes/config/back-office-user - EXIT PROCESS');
+if(!boconfig.ClientSecret) return cError('Missing ClientSecret for back office user from routes/config/back-office-user - EXIT PROCESS');
+if(!boconfig.scope) return cError('Missing scope for back office user from routes/config/back-office-user - EXIT PROCESS');
+cSuccess('Config correct - CONTINUE');
 
 
 return setBackOfficeToken()
@@ -29,7 +31,7 @@ return setBackOfficeToken()
     });
 
 function getOrdersAwaitingApproval(){
-    //orders that have been on hold for at least 48 hours but have not been approved
+    cLog('Getting orders that have been on hold for 48 hours and have no reminders sent');
     var now = new Date();
     now.setHours(now.getHours() - 48);
     var fortyEightHoursAgo = now.toISOString();
@@ -41,32 +43,48 @@ function getOrdersAwaitingApproval(){
             DateSubmitted: '<' + fortyEightHoursAgo,
             'xp.Over48': 'no' // user has not yet been reminded
         }
+    })
+    .then(function(orderList){
+        var orderids = _.pluck(orderList.Items, 'ID').join(',');
+        cSuccess(orderList.Meta.TotalCount + ' Orders Found', 'orderIDs: ' + orderids);
+        return orderList;
+    })
+    .catch(function(){
+        return cError('List call failed - EXIT PROCESS');
     });
 }
 
 function getApprovingUsers(orders){
-    // list of users for each order on hold that can approve it
+    cLog('Retrieve list of approving users for each order');
     var emailData = {};
     var approvalQueue = [];
     _.each(orders.Items, function(order){
         approvalQueue.push(function(){
             return OrderCloudSDK.Orders.ListApprovals('incoming', order.ID, {filters: {Status: 'Pending'}})
                 .then(function(approvals){
+                    cSuccess('Order approvals for ' + order.ID + ' retrieved');
                     var usersQueue = [];
                     _.each(approvals.Items, function(approval){
                         usersQueue.push(function(){
                             return OrderCloudSDK.Users.List(buyerid, {pageSize: 100, userGroupID: approval.ApprovingGroupID})
                                 .then(function(userList){
+                                    cSuccess('Users in approving group ' +  approval.ApprovingGroupID + 'retrieved');
                                     order = _.pick(order, ['ID', 'FromUser', 'FromUserID', 'DateSubmitted']);
                                     var recipients = _.pluck(userList.Items, 'Email');
                                     return emailData[order.ID] = {
                                         Order: order,
                                         Recipients: recipients
                                     };
+                                })
+                                .catch(function(){
+                                    cError('Failed to retrieve users in approving group ' + approval.ApprovingGroupID);
                                 });
                         }());
                     });
                     return $q.all(usersQueue);
+                })
+                .catch(function(){
+                    return cError('Failed to retrieve approvals for ' + order.ID);
                 });
         }());
     });
@@ -77,6 +95,7 @@ function getApprovingUsers(orders){
 }
 
 function emailUsers(emailData){
+    cLog('Builing up emails');
     var queue = [];
     _.each(emailData, function(email){
         var arrayRecipients = _.map(email.Recipients, function(email){
@@ -95,9 +114,20 @@ function emailUsers(emailData){
         };
         var template_content = [{name: 'main', content: 'content'}];
 
-        queue.push(mandrill_client.messages.sendTemplate({template_name: 'approval-over-48-hours', template_content: template_content, message: message}));
+        queue.push(function(){
+            return mandrill_client.messages.sendTemplate({template_name: 'approval-over-48-hours', template_content: template_content, message: message})
+                .then(function(){
+                    return cSuccess('Emails successfully sent to: ' + email.Recipients.join(','));
+                })
+                .catch(function(){
+                    return cError('Emails not sent to: ' + email.Recipients.join(','));
+                });
+        });
     });
-    return $q.all(queue);
+    return $q.all(queue)
+        .then(function(){
+            return cSuccess('All emails sent successfully - FINISH PROCESS');
+        });
 }
 
 function markComplete(emailData){
@@ -111,6 +141,7 @@ function markComplete(emailData){
 }
 
 function setBackOfficeToken(){
+    cLog('Retrieving back-office token');
     //TODO: there is a bug in javascript sdk that doesnt allow us
     //to use OrderCloudSDK.Auth.ClientCredentials log in. once this
     //is fixed replace with that call
@@ -125,13 +156,15 @@ function setBackOfficeToken(){
 
     request.post(requestBody, function (error, response, body) {
         if (error) {
+            cError('Failure - EXIT PROCESS');
             deferred.reject(error);
         } else {
             if(body && body.Errors && body.Errors.length){
                 var msg = body.Errors[0].Message;
-                console.log('Error Auth', msg);
+                cError('Auth Error - confirm back-office-user information is correct', msg);
                 deferred.reject(msg);
             } else {
+                cSuccess('Token Retrieved');
                 var token = JSON.parse(body)['access_token'];
                 OrderCloudSDK.SetToken(token);
                 deferred.resolve();
@@ -140,4 +173,16 @@ function setBackOfficeToken(){
     });
 
     return deferred.promise;
+}
+
+/* * * Logging Helpers * * */
+
+function cLog(message, code) {
+    console.log(chalk.magentaBright('[DFSS Scheduled Approval Reminder] ') + chalk.cyanBright(message), code ? code : '');
+}
+function cSuccess(message, code) {
+    console.log(chalk.magentaBright('[DFSS Scheduled Approval Reminder] ') + chalk.greenBright(message), code ? code : '');
+}
+function cError(message, code) {
+    console.log(chalk.magentaBright('[DFSS Scheduled Approval Reminder] ') + chalk.redBright(message), code ? code : '');
 }
